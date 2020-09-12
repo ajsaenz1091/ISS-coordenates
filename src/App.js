8da45538e22c8f055c72509a862a107114405537
Alberto Gonzalez <ajsaenz1091@gmail.com>
2020-09-12 00:44:46 -0400

    Fixing css

--- src/App.js
+++ src/App.js
@@ -16,9 +16,8 @@ class App extends Component {
   //This is a lifecycle method
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/todos/1') // fetch returns a promise
-    // .then(response => response.json()) // this returns a response that we need to return in a json format
-    // .then(coordenates => this.setState({iss: coordenates}));
-    .then(response => console.log(response));  // this returns an array of objects with the coordenates data in the api
+    .then(response => response.json()) // this returns a response that we need to return in a json format
+    .then(coordenates => this.setState({iss: coordenates}));  // this returns an array of objects with the coordenates data in the api
   }
 
   render() {
