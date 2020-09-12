0dab12d26801de58db3868cbf9cc98d1974aadd8
Alberto Gonzalez <ajsaenz1091@gmail.com>
2020-09-12 00:31:10 -0400

    testing fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Coordenates } from './components/coordenates.component';
+// const express = require('express');
 
 
 class App extends Component {
@@ -14,9 +15,10 @@ class App extends Component {
 
   //This is a lifecycle method
   componentDidMount() {
-    fetch('http://api.open-notify.org/iss-now.json') // fetch returns a promise
-    .then(response => response.json()) // this returns a response that we need to return in a json format
-    .then(coordenates => this.setState({iss: coordenates}));  // this returns an array of objects with the coordenates data in the api
+    fetch('https://jsonplaceholder.typicode.com/todos/1') // fetch returns a promise
+    // .then(response => response.json()) // this returns a response that we need to return in a json format
+    // .then(coordenates => this.setState({iss: coordenates}));
+    .then(response => console.log(response));  // this returns an array of objects with the coordenates data in the api
   }
 
   render() {
