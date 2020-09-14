5b51897830c09aaeca8cccfb9ea58c102d1690f8
Alberto Gonzalez <ajsaenz1091@gmail.com>
2020-09-14 16:58:09 -0400

    Updates

---
+++ precache-manifest.744c1dfa6f1cf95c4774a8c981f2de59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb4190273ec3b289de6c2d8bebb1c52",
+    "url": "/iss-coordenates/index.html"
+  },
+  {
+    "revision": "f96c83dc5df5b9e03890",
+    "url": "/iss-coordenates/static/css/main.05add8ca.chunk.css"
+  },
+  {
+    "revision": "44e8fa0fbccf839fa278",
+    "url": "/iss-coordenates/static/js/2.d615f2f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iss-coordenates/static/js/2.d615f2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96c83dc5df5b9e03890",
+    "url": "/iss-coordenates/static/js/main.b9bd1b96.chunk.js"
+  },
+  {
+    "revision": "18cba0406093fa5944e7",
+    "url": "/iss-coordenates/static/js/runtime-main.5dd5a82e.js"
+  },
+  {
+    "revision": "3c4ffbcbff218fcc13e54261a52df2ad",
+    "url": "/iss-coordenates/static/media/space.3c4ffbcb.jpg"
+  }
+]);
